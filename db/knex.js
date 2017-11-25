a6530a83497d36c4e338cc456a1153aacad59d08
Parker Lewis <parker.lewis14@att.net>
2017-11-25 14:38:04 -0600

    update knex filepath

--- db/knex.js
+++ db/knex.js
@@ -1,5 +1,5 @@
 const environment = process.env.NODE_ENV || 'development';
 const knexConfig = require('./knexfile')[environment];
-const knex = require('knex')(knexConfig);
+const knex = require('../node_modules/knex')(knexConfig);
 
 module.exports = knex;
