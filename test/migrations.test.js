20e623952a678f14a67778f10d78997e8a83a6c0
parkerlewis9 <parker.lewis14@att.net>
2018-03-18 09:08:02 -0500

    update circlci config

--- test/migrations.test.js
+++ test/migrations.test.js
@@ -4,7 +4,7 @@ const   mocha = require("mocha"),
         expect = require("chai").expect,
         knex = require("../knex");
 
-describe("Migration Tests", function () {
+describe("Migration Tests", () => {
     before((done) => {
         knex.migrate.latest()
             .then(() => done())
